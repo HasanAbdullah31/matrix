ddb19189d71d16da6008d505833aa8c8ae53fe9b
HasanAbdullah31 <37593075+HasanAbdullah31@users.noreply.github.com>
2018-03-23 02:01:07 -0400

    Change int* to float* and ask user for input

--- matrix.c
+++ matrix.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print(int* matrix, int rows, int cols) {
+void print(float* matrix, int rows, int cols) {
   for (int i=0; i<rows; ++i) {
     for (int j=0; j<cols; ++j)
-      printf("%d ",*(matrix+cols*i+j));   // matrix[i][j]
+      printf("%.1f  ",*(matrix+cols*i+j));   // matrix[i][j]
     printf("\n");
   }
 }
 
-int* multiply(int* A, int* B, int m, int n, int p) {
-  int* C=(int* )malloc(m*p*sizeof(int));
+float* multiply(float* A, float* B, int m, int n, int p) {
+  float* C=(float* )malloc(m*p*sizeof(float));
   for (int i=0; i<m; ++i)
     for (int j=0; j<n; ++j)
       for (int k=0; k<p; ++k)
@@ -20,13 +20,17 @@ int* multiply(int* A, int* B, int m, int n, int p) {
 
 int main(int argc, char** argv) {
   int m=4, n=4, p=4;
-  int* A=(int* )malloc(m*n*sizeof(int));
-  for (int i=0; i<m; ++i)
-    for (int j=0; j<n; ++j) *(A+n*i+j)=j;   // A[i][j]
-  int* B=(int* )malloc(n*p*sizeof(int));
+  float* A=(float* )malloc(m*n*sizeof(float));
+  for (int i=0; i<m; ++i) {
+    for (int j=0; j<n; ++j) {
+      printf("Enter A[%d][%d]: ",i,j);
+      scanf( "%f",&(*(A+n*i+j)) );   // A[i][j]
+    }
+  }
+  float* B=(float* )malloc(n*p*sizeof(float));
   for (int i=0; i<n; ++i)
-    for (int j=0; j<p; ++j) *(B+p*i+j)=(i==j)? 1:0;   // B[i][j]
-  int* C=multiply(A,B,m,n,p);
+    for (int j=0; j<p; ++j) *(B+p*i+j)=(i==j)? 1.0f:0.0f;   // B[i][j]
+  float* C=multiply(A,B,m,n,p);
   print(A,m,n), printf("\n");
   print(B,n,p), printf("\n");
   print(C,m,p);
