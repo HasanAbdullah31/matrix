830d805a2b281b244532ff5e367875e9348bca02
HasanAbdullah31 <37593075+HasanAbdullah31@users.noreply.github.com>
2018-03-23 01:43:23 -0400

    Implement matrix as single pointer in matrix.c

--- matrix.c
+++ matrix.c
@@ -1,44 +1,35 @@
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void print(int** matrix, int m, int n) {
-  for (int i=0; i<m; ++i) {
-    for (int j=0; j<n; ++j) printf("%d ",matrix[i][j]);
+void print(int* matrix, int rows, int cols) {
+  for (int i=0; i<rows; ++i) {
+    for (int j=0; j<cols; ++j)
+      printf("%d ",*(matrix+cols*i+j));   // matrix[i][j]
     printf("\n");
   }
 }
 
-int** multiply(int** A, int** B, int m, int n, int p) {
-  int** C=(int** )malloc(m*sizeof(int*));
-  for (int i=0; i<m; ++i) {
-    C[i]=(int* )malloc(p*sizeof(int));
+int* multiply(int* A, int* B, int m, int n, int p) {
+  int* C=(int* )malloc(m*p*sizeof(int));
+  for (int i=0; i<m; ++i)
     for (int j=0; j<n; ++j)
       for (int k=0; k<p; ++k)
-        C[i][k]+=A[i][j]*B[j][k];
-  }
+        *(C+p*i+k) += *(A+n*i+j) * (*(B+p*j+k));   // C[i][k]+=A[i][j]*B[j][k]
   return C;
 }
 
 int main(int argc, char** argv) {
-  srand(time(0));
   int m=4, n=4, p=4;
-  int** A=(int** )malloc(m*sizeof(int*));
-  for (int i=0; i<m; ++i) {
-    A[i]=(int* )malloc(n*sizeof(int));
-    for (int j=0; j<n; ++j) A[i][j]=(int)rand()%10;
-  }
-  int** B=(int** )malloc(n*sizeof(int*));
-  for (int i=0; i<n; ++i) {
-    B[i]=(int* )malloc(p*sizeof(int));
-    for (int j=0; j<p; ++j) B[i][j]=(i==j)? 1:0;
-  }
-  int** C=multiply(A,B,m,n,p);
+  int* A=(int* )malloc(m*n*sizeof(int));
+  for (int i=0; i<m; ++i)
+    for (int j=0; j<n; ++j) *(A+n*i+j)=j;   // A[i][j]
+  int* B=(int* )malloc(n*p*sizeof(int));
+  for (int i=0; i<n; ++i)
+    for (int j=0; j<p; ++j) *(B+p*i+j)=(i==j)? 1:0;   // B[i][j]
+  int* C=multiply(A,B,m,n,p);
   print(A,m,n), printf("\n");
   print(B,n,p), printf("\n");
   print(C,m,p);
-  for (int i=0; i<m; ++i) free(A[i]), free(C[i]);
-  for (int i=0; i<n; ++i) free(B[i]);
   free(A), free(B), free(C);
   return 0;
 }
